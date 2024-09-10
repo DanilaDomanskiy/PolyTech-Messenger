0171c291d7d55af8537b9fcb1ab8b8357c885779
Danila2005Domanskiy <daniladomanskiy@gmail.com>
2024-09-10 23:21:38 +0300

    Initial commit

---
+++ API/Web.API/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("User");
+        }
+    }
+}
